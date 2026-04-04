fdec79dd9d63e76bb3a3b1bd2fba4aedb53955b5
Souleymane Sirima Mbodj <sirimambodj@gmail.com>
2026-04-04 17:19:38 +0000

    Correction

--- Afficheur.java
+++ Afficheur.java
@@ -1,38 +1,30 @@
-/**
- * Afficheur.java
- * Auteur : Alioune Badara Barry
- * Rôle : Affichage console de la grille de Sudoku
- */
+// afficher console de la grille de Sudoku
 
 public class Afficheur {
-    /**
-     * Affiche la grille complète avec les bordures
-     * @param g la grille à afficher
-     */
+    // Affiche la grille complète avec les bordures
+
     public void afficherGrille(Grille g) {
-        System.out.println("+-------+-------+-------+");
+        System.out.println("╔═══════╦═══════╦═══════╗");
         for (int i = 0; i < 9; i++) {
             if (i > 0 && i % 3 == 0) {
-                System.out.println("+-------+-------+-------+");
+                System.out.println("╠═══════╬═══════╬═══════╣");
             }
             for (int j = 0; j < 9; j++) {
                 if (j % 3 == 0) {
-                    System.out.print("| ");
+                    System.out.print("║ ");
                 }
                 int val = g.getValeur(i, j);
-                System.out.print((val == 0) ? ". " : val + " ");
+                System.out.print((val == 0) ? "· " : val + " ");
                 if (j == 8) {
-                    System.out.println("|");
+                    System.out.println("║");
                 }
             }
         }
-        System.out.println("+-------+-------+-------+");
+        System.out.println("╚═══════╩═══════╩═══════╝");
     }
 
-    /**
-     * Affiche un message simple (titre, erreur, succes).
-     * @param msg le message
-     */
+    // Affiche un message simple (titre, erreur, succes).
+     
     public void afficherMessage(String msg) {
         System.out.println("\n*** " + msg + " ***\n");
     }
